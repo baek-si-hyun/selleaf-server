169381c7d34a60ed65c20b95e22b77b95b19ba1f
baek-si-hyun <qortlgus100@gmail.com>
2024-03-21 13:26:41 +0900

    init

--- selleaf/settings.py
+++ selleaf/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-xrbh@fs#!56dnm^+1ld6l5n5t7n)1d@6t(@r3a&2!_x1j8r*$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.35.214.147']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.35.214.147', 'selleaf.site']
 
 # Application definition
 
